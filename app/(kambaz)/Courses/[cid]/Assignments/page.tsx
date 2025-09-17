06b56a293b2d9d6a36530b508b0590cd14716687
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-09-17 14:46:45 -0400

    Fix all build errors: Update Assignments Links, Layout imports, Lab1 formatting

--- app/(kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,55 +1,52 @@
 import Link from "next/link";
 
 export default function Assignments() {
-    return (
-      <div id="wd-assignments">
-        <input placeholder="Search for Assignments"
-               id="wd-search-assignment" />
-        <button id="wd-add-assignment-group">+ Group</button>
-        <button id="wd-add-assignment">+ Assignment</button>
-        <h3 id="wd-assignments-title">
-          ASSIGNMENTS 40% of Total <button>+</button> </h3>
-        <ul id="wd-assignment-list">
-          <li className="wd-assignment-list-item">
-            <Link href="/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A1 - ENV + HTML
-            </Link>
+  return (
+    <div id="wd-assignments">
+      <input placeholder="Search for Assignments"
+             id="wd-search-assignment" />
+      <button id="wd-add-assignment-group">+ Group</button>
+      <button id="wd-add-assignment">+ Assignment</button>
+      <h3 id="wd-assignments-title">
+        ASSIGNMENTS 40% of Total <button>+</button>
+      </h3>
+      <ul id="wd-assignment-list">
+        <li className="wd-assignment-list-item">
+          <Link href="/Courses/1234/Assignments/123"
+                className="wd-assignment-link">
+            A1 - ENV + HTML
+          </Link>
           <div className="wd-assignment-details">
-            <span>Multiple Modules | <strong>Not available Until</strong>May 6 at 12:00am</span>
-            <span> Due may 13 at 11:59pm |</span>
-            <span> 100 pts</span>
+            <span>Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am | </span>
+            <span><strong>Due</strong> May 13 at 11:59pm | </span>
+            <span>100 pts</span>
           </div>
         </li>
 
         <li className="wd-assignment-list-item">
-          <Link
-            href="/Courses/1234/Assignments/124"
-            className="wd-assignment-link"
-          >
+          <Link href="/Courses/1234/Assignments/124"
+                className="wd-assignment-link">
             A2 - CSS + BOOTSTRAP
           </Link>
           <div className="wd-assignment-details">
-            <span>Multiple Modules |<strong>Not available Until</strong>May 13 at 12:00am</span>
-            <span> Due May 20 at 11:59pm |</span>
-            <span> 100 pts</span>
+            <span>Multiple Modules | <strong>Not available until</strong> May 13 at 12:00am | </span>
+            <span><strong>Due</strong> May 20 at 11:59pm | </span>
+            <span>100 pts</span>
           </div>
         </li>
 
         <li className="wd-assignment-list-item">
-          <Link
-            href="/Courses/1234/Assignments/125"
-            className="wd-assignment-link"
-          >
+          <Link href="/Courses/1234/Assignments/125"
+                className="wd-assignment-link">
             A3 - JAVASCRIPT + REACT
           </Link>
           <div className="wd-assignment-details">
-            <span>Multiple Modules | <strong>Not available Until</strong>May 20 at 12:00 am</span>
-            <span> Due May 27 at 11:59pm |</span>
-            <span> 100 pts</span>
+            <span>Multiple Modules | <strong>Not available until</strong> May 20 at 12:00am | </span>
+            <span><strong>Due</strong> May 27 at 11:59pm | </span>
+            <span>100 pts</span>
           </div>
         </li>
       </ul>
     </div>
-  );}
-  
+  );
+}
