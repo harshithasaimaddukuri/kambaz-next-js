7c3cb2cab22baea5300110258cfb2bdae6c109f2
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-09-17 15:57:30 -0400

    Fixed Lab1, images, imports, ready for deployment

--- app/(kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 export default function Assignments() {
   return (
     <div id="wd-assignments">
-      <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+      <input placeholder="Search for Assignments" id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
+
       <h3 id="wd-assignments-title">
         ASSIGNMENTS 40% of Total <button>+</button>
       </h3>
+
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/123"
-                className="wd-assignment-link">
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             A1 - ENV + HTML
           </Link>
           <div className="wd-assignment-details">
@@ -22,10 +22,8 @@ export default function Assignments() {
             <span>100 pts</span>
           </div>
         </li>
-
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/124"
-                className="wd-assignment-link">
+          <Link href="/Courses/1234/Assignments/124" className="wd-assignment-link">
             A2 - CSS + BOOTSTRAP
           </Link>
           <div className="wd-assignment-details">
@@ -34,10 +32,8 @@ export default function Assignments() {
             <span>100 pts</span>
           </div>
         </li>
-
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/125"
-                className="wd-assignment-link">
+          <Link href="/Courses/1234/Assignments/125" className="wd-assignment-link">
             A3 - JAVASCRIPT + REACT
           </Link>
           <div className="wd-assignment-details">
--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -5,7 +5,7 @@ export default function Lab1() {
     <div id="wd-lab1">
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
-      
+
       <div id="wd-h-tag">
         <h4>Heading Tags</h4>
         Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
@@ -14,29 +14,22 @@ export default function Lab1() {
       <div id="wd-p-tag">
         <h4>Paragraph Tag</h4>
         <p id="wd-p-1">
-          This is a paragraph. We often separate a long set of sentences with vertical spaces
-          to make the text easier to read. Browsers ignore vertical white spaces and render all
-          the text as one single set of sentences. To force the browser to add vertical spacing,
-          wrap the paragraphs you want to separate with the paragraph tag.
+          This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag.
         </p>
         <p id="wd-p-2">
-          This is the first paragraph. The paragraph tag is used to format
-          vertical gaps between long pieces of text like this one.
+          This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.
         </p>
         <p id="wd-p-3">
-          This is the second paragraph. Even though there is a deliberate white
-          gap between the paragraph above and this paragraph, by default
-          browsers render them as one contiguous piece of text as shown here on
-          the right.
+          This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by default browsers render them as one contiguous piece of text as shown here on the right.
         </p>
         <p id="wd-p-4">
-          This is the third paragraph. Wrap each paragraph with the paragraph
-          tag to tell browsers to render the gaps.
+          This is the third paragraph. Wrap each paragraph with the paragraph tag to tell browsers to render the gaps.
         </p>
       </div>
-      
+
       <div id="wd-lists">
         <h4>List Tags</h4>
+
         <h5>Ordered List Tag</h5>
         <p>How to make pancakes:</p>
         <ol id="wd-pancakes">
@@ -49,6 +42,7 @@ export default function Lab1() {
           <li>Flip and cook the other side.</li>
           <li>Serve and enjoy!</li>
         </ol>
+
         <p>My favorite recipe:</p>
         <ol id="wd-your-favorite-recipe">
           <li>Boil pasta in salted water</li>
@@ -57,7 +51,7 @@ export default function Lab1() {
           <li>Mix pasta with sauce</li>
           <li>Serve with parmesan cheese</li>
         </ol>
-        
+
         <h5>Unordered List Tag</h5>
         <p>My favorite books (in no particular order):</p>
         <ul id="wd-my-books">
@@ -77,7 +71,7 @@ export default function Lab1() {
           <li>Life of Pi by Yann Martel</li>
         </ul>
       </div>
-      
+
       <div id="wd-tables">
         <h4>Table Tag</h4>
         <table border={1} width="100%">
@@ -90,66 +84,16 @@ export default function Lab1() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Q1</td>
-              <td>HTML</td>
-              <td>2/3/21</td>
-              <td>85</td>
-            </tr>
-            <tr>
-              <td>Q2</td>
-              <td>CSS</td>
-              <td>2/10/21</td>
-              <td>90</td>
-            </tr>
-            <tr>
-              <td>Q3</td>
-              <td>JavaScript</td>
-              <td>2/17/21</td>
-              <td>95</td>
-            </tr>
-            <tr>
-              <td>Q4</td>
-              <td>React</td>
-              <td>2/24/21</td>
-              <td>88</td>
-            </tr>
-            <tr>
-              <td>Q5</td>
-              <td>Node.js</td>
-              <td>3/3/21</td>
-              <td>92</td>
-            </tr>
-            <tr>
-              <td>Q6</td>
-              <td>Express</td>
-              <td>3/10/21</td>
-              <td>87</td>
-            </tr>
-            <tr>
-              <td>Q7</td>
-              <td>MongoDB</td>
-              <td>3/17/21</td>
-              <td>94</td>
-            </tr>
-            <tr>
-              <td>Q8</td>
-              <td>Authentication</td>
-              <td>3/24/21</td>
-              <td>89</td>
-            </tr>
-            <tr>
-              <td>Q9</td>
-              <td>Deployment</td>
-              <td>3/31/21</td>
-              <td>91</td>
-            </tr>
-            <tr>
-              <td>Q10</td>
-              <td>Final Review</td>
-              <td>4/7/21</td>
-              <td>96</td>
-            </tr>
+            <tr><td>Q1</td><td>HTML</td><td>2/3/21</td><td>85</td></tr>
+            <tr><td>Q2</td><td>CSS</td><td>2/10/21</td><td>90</td></tr>
+            <tr><td>Q3</td><td>JavaScript</td><td>2/17/21</td><td>95</td></tr>
+            <tr><td>Q4</td><td>React</td><td>2/24/21</td><td>88</td></tr>
+            <tr><td>Q5</td><td>Node.js</td><td>3/3/21</td><td>92</td></tr>
+            <tr><td>Q6</td><td>Express</td><td>3/10/21</td><td>87</td></tr>
+            <tr><td>Q7</td><td>MongoDB</td><td>3/17/21</td><td>94</td></tr>
+            <tr><td>Q8</td><td>Authentication</td><td>3/24/21</td><td>89</td></tr>
+            <tr><td>Q9</td><td>Deployment</td><td>3/31/21</td><td>91</td></tr>
+            <tr><td>Q10</td><td>Final Review</td><td>4/7/21</td><td>96</td></tr>
           </tbody>
           <tfoot>
             <tr>
@@ -159,17 +103,28 @@ export default function Lab1() {
           </tfoot>
         </table>
       </div>
-      
+
       <div id="wd-images">
         <h4>Image tag</h4>
         Loading an image from the internet: <br />
-        <img id="wd-starship" width="400px" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship" />
+        <img
+          id="wd-starship"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          alt="Starship"
+          width={400}
+          height={300}
+        />
         <br />
-        Loading a local image:
-        <br />
-        <img id="wd-teslabot" src="https://cdn.mos.cms.futurecdn.net/nj3z7ieVrCcgyQkq6myNZ-1200-80.jpg" height="200px" alt="Tesla Bot" />
+        Loading a local image: <br />
+        <img
+          id="wd-teslabot"
+          src="https://cdn.mos.cms.futurecdn.net/nj3z7ieVrCcgyQkq6myNZ-1200-80.jpg"
+          alt="Tesla Bot"
+          width={300}
+          height={200}
+        />
       </div>
-      
+
       <div id="wd-forms">
         <h4>Form Elements</h4>
         <form id="wd-text-fields">
@@ -187,7 +142,7 @@ export default function Lab1() {
                  title="The last name"
                  id="wd-text-fields-last-name" />
           <br />
-          
+
           <h5>Text boxes</h5>
           <label>Biography:</label><br/>
           <textarea 
@@ -196,7 +151,7 @@ export default function Lab1() {
             rows={10}
             defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
           />
-          
+
           <h5 id="wd-buttons">Buttons</h5>
           <button type="button"
                   onClick={() => alert("Life is Good!")}
@@ -205,7 +160,7 @@ export default function Lab1() {
           </button>
           <br />
           <input type="file" id="wd-upload" />
-          
+
           <h5 id="wd-radio-buttons">Radio buttons</h5>
           <label>Favorite movie genre:</label><br />
           <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
@@ -216,7 +171,7 @@ export default function Lab1() {
           <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
           <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
           <label htmlFor="wd-radio-fantasy">Fantasy</label>
-          
+
           <h5 id="wd-checkboxes">Checkboxes</h5>
           <label>Favorite movie genre:</label><br/>
           <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
@@ -227,7 +182,7 @@ export default function Lab1() {
           <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
           <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
           <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
-          
+
           <h4 id="wd-dropdowns">Dropdowns</h4>
           <h5>Select one</h5>
           <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
@@ -237,7 +192,7 @@ export default function Lab1() {
             <option value="SCIFI">Science Fiction</option>
             <option value="FANTASY">Fantasy</option>
           </select>
-          
+
           <h5>Select many</h5>
           <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
           <select multiple id="wd-select-many-genre" defaultValue={["COMEDY","SCIFI"]}>
@@ -246,7 +201,7 @@ export default function Lab1() {
             <option value="SCIFI"> Science Fiction </option>
             <option value="FANTASY"> Fantasy </option>
           </select>
-          
+
           <h4>Other HTML field types</h4>
           <label htmlFor="wd-text-fields-email"> Email: </label>
           <input type="email"
@@ -261,7 +216,6 @@ export default function Lab1() {
           <input type="range"
                  defaultValue="4"
                  max="5"
-                 placeholder="Doe"
                  id="wd-text-fields-rating"/><br/>
           <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
           <input type="date"
@@ -269,7 +223,7 @@ export default function Lab1() {
                  id="wd-text-fields-dob"/><br/>
         </form>
       </div>
-      
+
       <h4>Anchor tag</h4>
       <p>
         Please <a href="https://www.lipsum.com" id="wd-lipsum">click here</a> to get dummy text<br/>
