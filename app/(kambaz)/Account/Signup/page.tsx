7565fbff682563647e8845d3295ef4fa8715ea31
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-09-17 22:05:29 -0400

    Fixed minor error

--- app/(kambaz)/Account/Signup/page.tsx
+++ app/(kambaz)/Account/Signup/page.tsx
@@ -1,30 +1,14 @@
 import Link from "next/link";
-
 export default function Signup() {
   return (
-    <div id="wd-Signup-screen">
+    <div id="wd-signup-screen">
       <h3>Sign up</h3>
-
-      <input
-        className="wd-username"
-        type="text"
-        defaultValue="alice123"
-      /><br/>
-
-      <input
-        className="wd-password"
-        type="password"
-        defaultValue="secretpw"
-      /><br/>
-
-      <input
-        className="wd-password-verify"
-        type="password"
-        defaultValue="secretpw"
-      /><br/>
-
-      <Link href="Profile"> Sign up </Link><br />
-      <Link href="Signin"> Sign in </Link>
+      <input placeholder="username" className="wd-username" /><br/>
+      <input placeholder="password" type="password" className="wd-password" /><br/>
+      <input placeholder="verify password"
+             type="password" className="wd-password-verify" /><br/>
+      <Link  href="Profile" > Sign up </Link><br />
+      <Link  href="Signin" > Sign in </Link>
     </div>
-  );
-}
+);}
+
