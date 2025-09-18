7c421696ed7c165379bdead5d0b4ddd722afcfbc
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-09-17 22:37:00 -0400

    added final changes

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,20 +3,21 @@ import Link from "next/link";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Labs</h1>
+      <h1>Labs</h1>  
+
       <ul>
         <li>
-          <Link href="/labs/lab1" id="wd-lab1-link">
+          <Link href="/Labs/Lab1" id="wd-lab1-link">
             Lab 1: HTML Examples
           </Link>
         </li>
         <li>
-          <Link href="/labs/lab2" id="wd-lab2-link">
+          <Link href="/Labs/Lab2" id="wd-lab2-link">
             Lab 2: CSS Basics
           </Link>
         </li>
         <li>
-          <Link href="/labs/lab3" id="wd-lab3-link">
+          <Link href="/Labs/Lab3" id="wd-lab3-link">
             Lab 3: JavaScript Fundamentals
           </Link>
         </li>
