608fc3a7847c39a94924eda8f6806c0bb80c7e44
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-10-30 02:14:06 -0400

    fix

--- app/Labs/Lab4/ReduxExamples/AddRedux/addReducer.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/addReducer.tsx
@@ -1,13 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-  sum: 0,
+sum: 0,
 };
 const addSlice = createSlice({
-  name: "add",
-  initialState,
-  reducers: {
-    add: (state, action) => {
-      state.sum = action.payload.a + action.payload.b;
+name: "addReducer",
+initialState,
+reducers: {
+add: (state, action) => {
+state.sum = action.payload.a + action.payload.b;
     },
   },
 });
