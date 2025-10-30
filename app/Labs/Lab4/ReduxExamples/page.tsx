ba027d8e81fd89dae2d739c12ec78a0cd27838af
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-10-30 01:27:21 -0400

    fix 6

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,11 +1,12 @@
-"use client"
-import HelloRedux from "./HelloRedux/page";
-import CounterRedux from "./CounterRedux/page";
-import AddRedux from "./AddRedux/page";
+"use client";
+
+import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 import TodoList from "./todos/TodoList";
 
 export default function ReduxExamples() {
-  return(
+  return (
     <div>
       <h2>Redux Examples</h2>
       <HelloRedux />
