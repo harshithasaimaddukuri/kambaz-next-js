ed38fdd2127b58da7d285d3b5ffce8eb06991ba2
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-10-30 00:48:43 -0400

    fixed minor error

--- app/(kambaz)/Dashboard/page.tsx
+++ app/(kambaz)/Dashboard/page.tsx
@@ -1,16 +1,37 @@
+//disable eslint for this file
+
 "use client";
-import { useState } from "react";
-import Link from "next/link";
+import { useState, ChangeEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewCourse, deleteCourse, updateCourse } from "../Courses/reducer";
-import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText, Button, FormControl } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+import Link from "next/link";
+import Image from "next/image";
+
+interface Course {
+  _id: string;
+  name: string;
+  number: string;
+  startDate: string;
+  endDate: string;
+  image: string;
+  description: string;
+}
+
+interface RootState {
+  coursesReducer: {
+    courses: Course[];
+  };
+}
 
 export default function Dashboard() {
-  const { courses } = useSelector((state: any) => state.coursesReducer);
   const dispatch = useDispatch();
 
-  const [course, setCourse] = useState<any>({
-    _id: "0",
+  const courses: Course[] = useSelector((state: RootState) => state.coursesReducer.courses);
+
+  const [course, setCourse] = useState<Course>({
+    _id: uuidv4(),
     name: "New Course",
     number: "New Number",
     startDate: "2023-09-10",
@@ -19,81 +40,77 @@ export default function Dashboard() {
     description: "New Description",
   });
 
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setCourse({ ...course, [name]: value });
+  };
+
   return (
     <div id="wd-dashboard" className="p-4">
-      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <h1>Dashboard</h1>
       <hr />
+
       <h5>
         New Course
         <button
           className="btn btn-primary float-end"
-          onClick={() => dispatch(addNewCourse(course))}
-          id="wd-add-new-course-click"
+          onClick={() => dispatch(addNewCourse({ ...course, _id: uuidv4() }))}
         >
           Add
         </button>
         <button
           className="btn btn-warning float-end me-2"
           onClick={() => dispatch(updateCourse(course))}
-          id="wd-update-course-click"
         >
           Update
         </button>
       </h5>
-      <br />
+
       <FormControl
+        name="name"
         value={course.name}
         className="mb-2"
-        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+        onChange={handleInputChange}
         placeholder="Course Name"
       />
       <FormControl
         as="textarea"
+        name="description"
         value={course.description}
-        rows={3}
-        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+        onChange={handleInputChange}
         placeholder="Course Description"
+        className="mb-2"
       />
+
       <hr />
-      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
-      <Row xs={1} md={5} className="g-4 mt-2">
-        {courses.map((c: any) => (
-          <Col key={c._id}>
-            <Card className="h-100">
-              <Link href={`/Courses/${c._id}/Home`} className="text-decoration-none text-dark">
-                <CardImg
-                  src={c.image}
-                  variant="top"
-                  width="100%"
-                  height={160}
-                  alt={c.name}
-                />
-                <CardBody>
-                  <CardTitle className="text-nowrap overflow-hidden">
-                    {c.name}
-                  </CardTitle>
-                  <CardText className="overflow-hidden" style={{ height: "100px" }}>
-                    {c.description}
-                  </CardText>
-                  <Button variant="primary" className="w-100">
-                    Go
-                  </Button>
-                </CardBody>
-              </Link>
-              <div className="p-2 d-flex justify-content-between">
+      <div className="row row-cols-1 row-cols-md-5 g-4">
+        {courses.map((c: Course) => ( 
+          <div key={c._id} className="col">
+            <div className="card">
+              <Image
+                src={c.image}
+                alt={c.name}
+                width={300}
+                height={200}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{c.name}</h5>
+                <p className="card-text">{c.description}</p>
+                <Link href={`/Kambaz/Courses/${c._id}`}>
+                  <button className="btn btn-primary">Go</button>
+                </Link>
                 <button
-                  className="btn btn-warning me-2 flex-grow-1"
-                  id="wd-edit-course-click"
+                  className="btn btn-warning ms-2"
                   onClick={(e) => {
                     e.preventDefault();
-                    setCourse(c); 
+                    setCourse(c);
                   }}
                 >
                   Edit
                 </button>
                 <button
-                  className="btn btn-danger flex-grow-1"
-                  id="wd-delete-course-click"
+                  className="btn btn-danger float-end"
                   onClick={(e) => {
                     e.preventDefault();
                     dispatch(deleteCourse(c._id));
@@ -102,10 +119,10 @@ export default function Dashboard() {
                   Delete
                 </button>
               </div>
-            </Card>
-          </Col>
+            </div>
+          </div>
         ))}
-      </Row>
+      </div>
     </div>
   );
 }
