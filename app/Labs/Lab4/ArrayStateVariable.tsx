84a27074c0f41f9e645f9301940d336654492d9f
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-11-04 18:42:51 -0500

    fixed lab

--- app/Labs/Lab4/ArrayStateVariable.tsx
+++ app/Labs/Lab4/ArrayStateVariable.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { useState } from "react";
-import { useSelector } from "react-redux";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function ArrayStateVariable() {
   const [array, setArray] = useState([1, 2, 3, 4, 5]);
@@ -16,26 +14,35 @@ export default function ArrayStateVariable() {
   
   return (
     <div id="wd-array-state-variables">
-      <h2>Array State Variable</h2>
-      <button 
-        className="btn btn-success mb-3"
-        onClick={addElement}
-      >
-        Add Element
-      </button>
-      <ul>
-        {array.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button 
-              className="btn btn-danger btn-sm ms-2"
-              onClick={() => deleteElement(index)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="card">
+        <div className="card-body">
+          <h2>Array State Variable</h2>
+          
+          <button 
+            onClick={addElement} 
+            className="btn btn-success mb-2"
+          >
+            Add Element
+          </button>
+          
+          <ul className="list-group">
+            {array.map((item, index) => (
+              <li 
+                key={index} 
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                {item}
+                <button 
+                  onClick={() => deleteElement(index)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <hr />
     </div>
   );
